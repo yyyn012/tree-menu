3bd9bc69d23906e62081fa9532656c880fc1bd55
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-03-26 23:26:59 +0900

    test

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-// $(document).ready(function () {
-//   $(".menu>a").click(function () {
-//     $(this).next("ul").toggleClass("hide");
-//     var submenu = $(this).next("ul");
-//     if (submenu.is(":visible")) {
-//       submenu.show();
-//     } else {
-//       if (submenu.is(":invisible")) {
-//         submenu.hide();
-//       }
-//     }
-//   });
-// });
+var smenu; //현재 보여지고 있는 메뉴
+function showmenu(divId) {
+  //현재 보여지는 div 메뉴는 닫기
+  if (smenu != null) smenu.style.display = "none";
+  // 새 메뉴 펼치기
+  var menu = document.getElementById(divId);
 
-// $(document).ready(function () {
-//   $(".menu-btn").on("click", function () {
-//     $(this).next().toggleClass("hide");
-//   });
-// });
-
-const menuBtn = document.querySelectorAll(".menu-btn");
-const hiddenMenu = document.getElementsByClassName(".hide");
-
-function toggleMenu() {
-  if (hiddenMenu.classList.contains("hide")) {
-    hiddenMenu.classList.remove("hide");
-    hiddenMenu.classList.add("open");
-  } else {
-    hiddenMenu.classList.remove("open");
-    hiddenMenu.classList.add("hide");
-  }
+  menu.style.display = "block";
+  //다시 설정
+  smenu = menu;
 }

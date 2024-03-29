3fcb6d9d78bebd3774f89f3ffdba967553bec45d
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-03-29 21:54:35 +0900

    menu test

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const list = document.querySelectorAll(".list > li");
+const content = document.querySelectorAll(".content");
 const listBtn = document.querySelectorAll(".btn");
 
-function Menu() {
-  for (let i = 0; i < list.length; i++) {
-    list[i].listBtn.addEventListener("click", toggleMenu);
-  }
-}
+listBtn.forEach((element) => {
+  element.addEventListener("click", toggleMenu);
+});
 
 function toggleMenu() {
-  if (list[i].classList.contains("on")) {
-    list[i].classList.remove("on");
-  } else {
-    list[i].classList.add("on");
+  for (let i = 0; i < list.length; i++) {
+    const id = listBtn[i].id;
+    console.log(id);
   }
 }

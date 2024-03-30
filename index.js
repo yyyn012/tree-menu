5fb54f2be4ea4ccdea48aeaf192a9305c4d7df77
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-03-30 22:00:33 +0900

    tree menu

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const list = document.querySelectorAll(".list > li");
+// const list = document.querySelector(".list");
+// const listContent = document.querySelectorAll(".list > li");
+// const listBtn = document.querySelectorAll(".btn");
+// const content = document.querySelector(".content > li");
+
+// listBtn.forEach((element) => {
+//   element.addEventListener("click", () => {
+//     for (let i = 0; i < listBtn.length; i++) {
+//       console.log(listBtn[0]);
+//     }
+//   });
+// });
+
+const tabBtns = document.querySelectorAll(".btn");
 const content = document.querySelectorAll(".content");
-const listBtn = document.querySelectorAll(".btn");
+const onConts = document.querySelectorAll(".on");
 
-listBtn.forEach((element) => {
-  element.addEventListener("click", toggleMenu);
+tabBtns.forEach((btn, index) => {
+  btn.addEventListener("click", (e) => {
+    if (onConts[index].classList.contains("on")) {
+      onConts[index].classList.remove("on");
+    } else {
+      onConts[index].classList.add("on");
+    }
+  });
 });
-
-function toggleMenu() {
-  for (let i = 0; i < list.length; i++) {
-    const id = listBtn[i].id;
-    console.log(id);
-  }
-}
